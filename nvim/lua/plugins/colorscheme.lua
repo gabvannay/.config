522b4ca26e216580cc632f778144723d94ace486
Gabriel Vannay <82763825+gabvannay@users.noreply.github.com>
2024-11-24 08:47:54 +0100

    new: neovim configs

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,19 @@
+return {
+    {
+        "LazyVim/LazyVim",
+        opts = { colorscheme = "tokyonight" },
+    },
+    -- {
+    --     "ayu-theme/ayu-vim",
+    -- },
+    -- {
+    --     "lunarvim/horizon.nvim",
+    --     opts = {
+    --         transparent = true,
+    --         styles = {
+    --             sidebars = "transparent",
+    --             float = "transparent",
+    --         },
+    --     },
+    -- },
+}
---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+        ensure_installed = {
+            "python",
+            "go",
+            "javascript",
+            "markdown",
+            "markdown_inline",
+        },
+    },
+}
